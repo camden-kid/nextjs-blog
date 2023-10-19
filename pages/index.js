275df4c2409e0dbd52cd539b1a0c7b355fb0c43d
Pinakin Desai <pinakin.desai@gmail.com>
2023-10-19 10:52:25 +0100

    Setup fro AWS

--- pages/index.js
+++ pages/index.js
@@ -3,42 +3,64 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData();
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
+
+// export default function Home({ allPostsData }) {
+//   return (
+//     <Layout home>
+//       <Head>
+//         <title>{siteTitle}</title>
+//       </Head>
+//       <section className={utilStyles.headingMd}>
+//         <p>[Your Self Introduction]</p>
+//         <p>
+//           (This is a sample website - you’ll be building a site like this on{" "}
+//           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+//         </p>
+//       </section>
+//       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+//         <h2 className={utilStyles.headingLg}>Blog</h2>
+//         <ul className={utilStyles.list}>
+//           {allPostsData.map(({ id, date, title }) => (
+//             <li className={utilStyles.listItem} key={id}>
+//               {title}
+//               <br />
+//               {id}
+//               <br />
+//               {date}
+//             </li>
+//           ))}
+//         </ul>
+//       </section>
+//     </Layout>
+//   );
+// }
+
+export default function Home({ formattedDate }) {
+  return (
+    <>
+      <h1>Static page</h1>
+      <p>This page is static. It was built on {formattedDate}.</p>
+      <p>
+        <a href="/ssr">View a server-side rendered page.</a>
+      </p>
+    </>
+  );
 }
 
-export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  );
+export async function getStaticProps() {
+  const buildDate = Date.now();
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "long",
+    timeStyle: "long",
+  }).format(buildDate);
+
+  return { props: { formattedDate } };
 }

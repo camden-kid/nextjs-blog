ed30dd88d7dd25c6cf7508810fe1c4e23682070f
Pinakin Desai <pinakin.desai@gmail.com>
2023-11-20 16:12:43 +0000

    Remove QueryResponse

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,13 @@
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
 export default function Blog() {
-  return <>Blog</>
+  return (
+    <Box component="form">
+      <div>
+        <TextField required id="outlined-required" label="Required" defaultValue="Hello World" />
+        <TextField disabled id="outlined-disabled" label="Disabled" defaultValue="Hello World" />
+      </div>
+    </Box>
+  );
 }

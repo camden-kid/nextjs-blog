5e07ad0381d7a494f4e644c110c80e71e63b8533
Pinakin Desai <pinakin.desai@gmail.com>
2023-11-29 16:28:03 +0000

    Added save alert

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,10 +7,12 @@ import Typography from "@mui/material/Typography";
 import { SyntheticEvent, useState } from "react";
 import { GetAllPostsDocument, useAddPostMutation } from "../generated/graphql";
 import { useMain } from "../_contexts/main/MainContext";
+import Alert from "@mui/material/Alert";
 
 export default function Blog() {
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
   const [addPost, { loading, error, data }] = useAddPostMutation({
     refetchQueries: [GetAllPostsDocument],
   });
@@ -27,6 +29,7 @@ export default function Blog() {
         created: `${date.substring(0, 10)} ${date.substring(11, 22)}`,
       },
     });
+    setShowAlert(true)
   };
 
   return (
@@ -34,11 +37,28 @@ export default function Blog() {
       <Typography variant="h5" component="div">
         Add new post
       </Typography>
-      <TextField fullWidth required label="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
-      <TextField fullWidth required label="Text" value={text} onChange={(e) => setText(e.target.value)} />
-      <Button type="submit" variant="contained" disabled={!title || !text}>
+      <TextField
+        fullWidth
+        required
+        label="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        disabled={loading}
+        onClick={() => setShowAlert(false)}
+      />
+      <TextField
+        fullWidth
+        required
+        label="Text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        disabled={loading}
+        onClick={() => setShowAlert(false)}
+      />
+      <Button type="submit" variant="contained" disabled={!title || !text || loading}>
         Submit
       </Button>
+      {showAlert && data && <Alert severity="success">Post added</Alert>}
     </Stack>
   );
 }
